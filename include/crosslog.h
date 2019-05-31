b1a4f1c94bf4d4fe03b9ec896e4d0646acbf85e5
M1cha <sigmaepsilon92@gmail.com>
2019-05-31 10:43:48 +0200

    add CROSSLOGV

--- include/crosslog.h
+++ include/crosslog.h
@@ -12,6 +12,7 @@
     #define CROSSLOG_INTERNAL(prefix, fmt, ...) \
         printk(prefix " " CROSSLOG_TAG ": " fmt "\n", ##__VA_ARGS__)
 
+    #define CROSSLOGV(fmt, ...) CROSSLOG_INTERNAL("V", fmt, ##__VA_ARGS__)
     #define CROSSLOGD(fmt, ...) CROSSLOG_INTERNAL("D", fmt, ##__VA_ARGS__)
     #define CROSSLOGI(fmt, ...) CROSSLOG_INTERNAL("I", fmt, ##__VA_ARGS__)
     #define CROSSLOGW(fmt, ...) CROSSLOG_INTERNAL("W", fmt, ##__VA_ARGS__)
@@ -23,6 +24,7 @@
     #include <esp_log.h>
     #include <stdlib.h>
 
+    #define CROSSLOGV(fmt, ...) ESP_LOGV(CROSSLOG_TAG, fmt, ##__VA_ARGS__)
     #define CROSSLOGD(fmt, ...) ESP_LOGD(CROSSLOG_TAG, fmt, ##__VA_ARGS__)
     #define CROSSLOGI(fmt, ...) ESP_LOGI(CROSSLOG_TAG, fmt, ##__VA_ARGS__)
     #define CROSSLOGW(fmt, ...) ESP_LOGW(CROSSLOG_TAG, fmt, ##__VA_ARGS__)
@@ -43,6 +45,7 @@
     #define CROSSLOG_COLOR_GREEN   CROSSLOG_COLOR_PREFIX "40m"
     #define CROSSLOG_COLOR_ORANGE  CROSSLOG_COLOR_PREFIX "202m"
     #define CROSSLOG_COLOR_RED     CROSSLOG_COLOR_PREFIX "196m"
+    #define CROSSLOG_COLOR_WHITE   CROSSLOG_COLOR_PREFIX "15m"
     #define CROSSLOG_COLOR(x) (isatty(STDERR_FILENO) ? CROSSLOG_COLOR_ ## x : "")
 
     #define CROSSLOG_INTERNAL(prefix, color, fmt, ...) \
@@ -50,6 +53,7 @@
             CROSSLOG_COLOR(color), ##__VA_ARGS__, CROSSLOG_COLOR(RESET) \
         )
 
+    #define CROSSLOGV(fmt, ...) CROSSLOG_INTERNAL("V", WHITE, fmt, ##__VA_ARGS__)
     #define CROSSLOGD(fmt, ...) CROSSLOG_INTERNAL("D", BLUE, fmt, ##__VA_ARGS__)
     #define CROSSLOGI(fmt, ...) CROSSLOG_INTERNAL("I", GREEN, fmt, ##__VA_ARGS__)
     #define CROSSLOGW(fmt, ...) CROSSLOG_INTERNAL("W", ORANGE, fmt, ##__VA_ARGS__)
